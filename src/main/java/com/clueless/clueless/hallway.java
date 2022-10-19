d3a611c56c9b9ef4de7258854f2fe22af8fdc2e3
Yamil <yamil.irizarry@upr.edu>
2022-10-19 19:16:42 -0400

    Created location classes and player class

---
+++ src/main/java/com/clueless/clueless/hallway.java
@@ -0,0 +1,35 @@
+package com.clueless.clueless;
+
+public class hallway extends location{
+    player occupants; //hallway only holds one player, kept plural for consistency
+    location[] adjLocations = new location[2]; //hallways connect 2 rooms
+
+    public hallway(String name, String codename){
+        super(name, codename);
+    }
+
+    public void setAdjacent(location[] locations){ //sets adjacent locations
+        adjLocations = locations;
+    }
+
+   public location[] getAdjacent(){
+        return adjLocations;
+   }
+
+   public Boolean addOccupant(player occupant){ //True if succesfull, false if fail
+        if (occupants != null){
+            occupants = occupant;
+            return true;
+        }
+        return false;
+   }
+
+   public Boolean removeOccupant(player occupant){ //True if removed, false if hallway empty or player not in hallway
+        if(occupants != occupant || occupants == null){
+            return false;
+        }
+        occupants = null;
+        return true;
+   }
+    
+}
---
+++ src/main/java/com/clueless/clueless/location.java
@@ -0,0 +1,23 @@
+package com.clueless.clueless;
+
+//base location class
+public class location {
+    // declare variables
+    String name, codename;
+    boolean location_occupied = false;
+
+    // constructors for location, location_occupied,
+    public location(String name, String codename) {
+        this.name = name;
+        this.codename = codename;
+    }
+
+    public void setOccupied(boolean location_occupied) {
+        this.location_occupied = location_occupied;
+    }
+
+    public boolean isOccupied() {
+        return location_occupied;
+    }
+  
+}
