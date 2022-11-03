2726232cffd921eb0db2f49f0e052a93a5fd60e2
jenhuang01 <76776961+jenhuang01@users.noreply.github.com>
2022-11-02 22:59:46 -0700

    Add files via upload

---
+++ src/main/java/com/clueless/clueless/caseFile.java
@@ -0,0 +1,18 @@
+package com.clueless_game.clueless;
+
+public class caseFile {
+    card player, weapon, room;
+
+    // constructor
+    public caseFile(card player, card weapon, card room) {
+        this.player = player;
+        this.weapon = weapon;
+        this.room = room;
+    }
+
+    // reveal method
+    public card[] reveal() {
+        card[] caseFile_cards = { player, weapon, room };
+        return caseFile_cards;
+    }
+}
