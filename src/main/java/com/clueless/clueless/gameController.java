795d7472555d3ce4be2806fb33f64e497409440a
Yamil <yamil.irizarry@upr.edu>
2022-11-07 17:15:38 -0400

    Testing views are working

--- src/main/java/com/clueless/clueless/gameController.java
+++ src/main/java/com/clueless/clueless/gameController.java
@@ -1,27 +1,50 @@
 package com.clueless.clueless;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class gameController {
     // server subclass to interface between client subsystem and the rest of the
     // backend
 
-    @GetMapping("/")
-	public String index() {
-		return "Welcome to the ClueLess Backend!";
-	}
+    @RequestMapping(value = "/index")
+    public String home(ModelMap model) {
 
-    @GetMapping("/newgame")
+        return "index";
+    }
+
+    @RequestMapping(value = "/show")
+    public ModelAndView show() {
+
+        var mav = new ModelAndView();
+
+        var now = LocalDateTime.now();
+        var formatter = DateTimeFormatter.ISO_DATE_TIME;
+        var dateTimeFormatted = formatter.format(now);
+
+        mav.addObject("now", dateTimeFormatted);
+        mav.setViewName("show");
+
+        return mav;
+    }
+
+    @RequestMapping("/newgame")
     public String newGame() {
         return "Start a new game.";
     }
 
-    @GetMapping("/myid") //test
+    @RequestMapping("/myid") //test
     public String myId(HttpSession session){
         return "My id is" + session.getId();
     }
